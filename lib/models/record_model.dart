83db503ba25decfe15be6a67363e6f91aa85eb9f
Murat <104165687+gczmurat@users.noreply.github.com>
2024-02-29 00:10:39 +0300

    model

--- lib/models/record_model.dart
+++ lib/models/record_model.dart
@@ -1,17 +1,16 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:flutter/foundation.dart';
 
-part 'record.freezed.dart';
-part 'record.g.dart';
-
+part 'user.freezed.dart';
+part 'user.g.dart';
 @freezed
 class Record with _$Record {
   const factory Record({
-    required double weight, // Tip belirtimi eklendi
-    required DateTime dateTime, // Tip belirtimi eklendi
-    required String note, // Tip belirtimi eklendi
-    String? photoUrl, // Nullable olduğu için doğru
+    required double weight,
+    required DateTime dateTime,
+    String? note,
+    String? photoUrl,
   }) = _Record;
 
-  factory Record.fromJson(Map<String, dynamic> json) => _$RecordFromJson(json); // Object? yerine dynamic kullanıldı
+  factory Record.fromJson(Map<String, Object?> json)
+      => _$RecordFromJson(json);
 }
